2e9f2e73f15c03ed3be75f893e32f02175f16682
Gustavo Ferreira <gustavoaferreira97@gmail.com>
2026-01-09 15:57:43 -0300

    correção swagger

--- Fcg.Payments.Api/Setup/WebApplicationExtensions.cs
+++ Fcg.Payments.Api/Setup/WebApplicationExtensions.cs
@@ -2,6 +2,7 @@
 using Fcg.Payments.Api.Api.Middleware;
 using Fcg.Payments.Api.Infra;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using System.Text.Json;
@@ -12,36 +13,35 @@ namespace Fcg.Payments.Api.Setup
     {
         public static WebApplication UseApiCore(this WebApplication app)
         {
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedHost | ForwardedHeaders.XForwardedProto,
+                KnownNetworks = { },
+                KnownProxies = { }
+            });
+
             app.UseMiddleware<ErrorMiddleware>();
             app.UseMiddleware<RequestLoggingMiddleware>();
             app.UseSwagger(c =>
             {
                 c.PreSerializeFilters.Add((swagger, httpReq) =>
                 {
-                    var prefix = httpReq.Headers["X-Forwarded-Prefix"].FirstOrDefault();
+                    var proto = httpReq.Headers["X-Forwarded-Proto"].FirstOrDefault() ?? httpReq.Scheme;
+                    var host = httpReq.Headers["X-Forwarded-Host"].FirstOrDefault() ?? httpReq.Host.Value;
 
-                    // Quando vem via gateway, prefix será "/games" ou "/users" etc.
-                    // Quando acessa direto o container (interno), prefix estará vazio.
-                    if (!string.IsNullOrWhiteSpace(prefix))
+                    var basePath = app.Configuration["ReverseProxyBasePath"] ?? "";
+                    if (!string.IsNullOrWhiteSpace(basePath) && !basePath.StartsWith("/"))
+                        basePath = "/" + basePath;
+
+                    swagger.Servers = new List<OpenApiServer>
                     {
-                        swagger.Servers = new List<OpenApiServer>
-                        {
-                            new() { Url = prefix } // URL relativa ao host atual (gateway)
-                        };
-                    }
-                    else
-                    {
-                        // Acesso direto (sem gateway): mantém raiz
-                        swagger.Servers = new List<OpenApiServer>
-                        {
-                            new() { Url = "/" }
-                        };
-                    }
+                        new() { Url = $"{proto}://{host}{basePath}" }
+                    };
                 });
             });
             app.UseSwaggerUI(opt =>
             {
-                opt.SwaggerEndpoint("v1/swagger.json", "FIAP Cloud Games v1");
+                opt.SwaggerEndpoint("v1/swagger.json?v=20260109-1", "FIAP Cloud Games v1");
                 opt.DisplayRequestDuration();
             });
 
